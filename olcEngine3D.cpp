126af8789e3148ae1ed32e0da3c75f15b38ce98e
sanggon6107 <sanggon6107@naver.com>
2022-06-24 22:04:29 +0900

    olcConsoleGameEngine 상속

--- olcEngine3D.cpp
+++ olcEngine3D.cpp
@@ -3,7 +3,35 @@
 
 
 
+
+class olcEngine3D : public olcConsoleGameEngine
+{
+public :
+	olcEngine3D()
+	{
+		m_sAppName = L"3D Demo"; // enable Unicode.
+	}
+
+	bool OnUserCreate() override
+	{
+		return true;
+	}
+
+	bool OnUserUpdate(float elapsed_time) override
+	{
+		return true;
+	}
+};
+
+
+
 int main()
 {
 
+
+	olcEngine3D demo;
+	if (demo.ConstructConsole(256, 240, 4, 4))
+	{
+		demo.Start();
+	}
 }
